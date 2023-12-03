3d8f4426ea12d6491d25de10fea223adf8895955
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2023-12-03 14:14:07 +0100

    Add .gitgnore + Begining of components implementations

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput } from "react-native";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      {/* TODO: BUTTONS */}
+    </View>
+  );
+}
+
+export default StartGameScreen;
