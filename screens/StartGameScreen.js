1fd0893ccbf0f1e0a535c19fe835f2588c27b20d
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2023-12-05 17:06:00 +0100

    Lesson 49 + 50

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,29 @@
-import { TextInput } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
-      {/* TODO: BUTTONS */}
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
