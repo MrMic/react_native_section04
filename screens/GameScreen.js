2723d17f93718c0f99f3a3be3eec77820090b495
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2023-12-19 16:42:14 +0100

    Implementation of switching to GameScreen

---
+++ screens/GameScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function GameScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Game Screen</Text>
+    </View>
+  );
+}
+
+export default GameScreen;
+
+const styles = StyleSheet.create({});
